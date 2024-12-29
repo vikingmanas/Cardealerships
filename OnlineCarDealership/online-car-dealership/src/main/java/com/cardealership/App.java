8bc6f25e45cb8820390141d42fe93daa897a5636
vikingmanas <147379602+vikingmanas@users.noreply.github.com>
2024-12-29 12:20:22 +0530

    deleted

--- OnlineCarDealership/online-car-dealership/src/main/java/com/cardealership/App.java
+++
@@ -1,8 +0,0 @@
-package com.cardealership;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Online Car Dealership!");
-        // Entry point of the application
-    }
-}
--- OnlineCarDealership/online-car-dealership/src/test/java/com/cardealership/CarDAOTest.java
+++
@@ -1,5 +0,0 @@
-package com.cardealership;
-
-public class CarDAOTest {
-    
-}
